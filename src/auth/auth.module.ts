4347e12417b47ade420df7c9a576d53da97d2dbf
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-01-04 12:58:11 +0530

    added auth route

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModel } from '../models/auth.model';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
+    MongooseModule.forFeature([{ name: 'User', schema: UserModel }]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { SignUpDto } from 'src/dtos/SignUp.dto';
+import { UserSchema } from 'src/models/auth.model';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel('UserSchema') private readonly userModel: Model<UserSchema>,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto) {
+    const { email, userName, password } = signUpDto;
+    const user = await this.userModel.findOne({ email });
+    if (user) {
+      throw new BadRequestException('User already exists');
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new this.userModel({
+      email,
+      userName,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    return { message: 'User created successfully' };
+  }
+}
