96727b5fd8d02ce5ef78facd4971c0f43bf6e8af
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-01-05 00:52:23 +0530

    create login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from 'src/dtos/SignUp.dto';
+import { LogInDto } from 'src/dtos/LogIn.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/signup')
-  SignUp(@Body() signUpDto: SignUpDto) {
+  SignUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
     return this.authService.signup(signUpDto);
+  }
+
+  @Post('/login')
+  Login(@Body() logInDto: LogInDto): Promise<{ token: string }> {
+    return this.authService.login(logInDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { Model } from 'mongoose';
 import { SignUpDto } from 'src/dtos/SignUp.dto';
 import { User } from 'src/models/auth.model';
 import * as bcrypt from 'bcryptjs';
+import { LogInDto } from 'src/dtos/LogIn.dto';
 
 @Injectable()
 export class AuthService {
@@ -13,7 +14,7 @@ export class AuthService {
     private readonly jwtServie: JwtService,
   ) {}
 
-  async signup(signUpDto: SignUpDto) {
+  async signup(signUpDto: SignUpDto): Promise<{ token: string }> {
     const { email, password, userName } = signUpDto;
     const existingUser = await this.userModel.findOne({ email });
     if (existingUser) {
@@ -33,6 +34,26 @@ export class AuthService {
 
     const payload = { userId: newUser._id };
 
+    const token = this.jwtServie.sign(payload);
+    return { token };
+  }
+
+  async login(logInDto: LogInDto): Promise<{ token: string }> {
+    const { emailOrUserName, password } = logInDto;
+
+    const user = await this.userModel.findOne({
+      $or: [{ email: emailOrUserName }, { userName: emailOrUserName }],
+    });
+
+    if (!user) {
+      throw new BadRequestException('no user found on this email or user name');
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      throw new BadRequestException('Invalid credentials');
+    }
+    const payload = { userId: user._id };
     const token = this.jwtServie.sign(payload);
     return { token };
   }
