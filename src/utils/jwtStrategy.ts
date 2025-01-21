dcfafadc3e3cfa776e82aca11b3f66c8806bfd59
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-01-21 22:09:34 +0530

    updated the packages

--- src/utils/jwtStrategy.ts
+++ src/utils/jwtStrategy.ts
@@ -7,7 +7,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpression: true,
+      ignoreExpiration: true,
       secretOrKey: process.env.JWT_SECRET_KEY,
     });
   }
